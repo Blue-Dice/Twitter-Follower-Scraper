20e4575dadc571dadee5923915ade4c5b2a41171
Dhairya-Kumar-Sharma <dhairya.hestabit@gmail.com>
2023-02-01 12:26:34 +0530

    mac support and code optimization

--- Scraper/TwitterScraper.py
+++ Scraper/TwitterScraper.py
@@ -1,18 +1,21 @@
-import pandas as pd
-from selenium import webdriver
+import multiprocessing
+multiprocessing.freeze_support()
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.service import Service as ChromeService
-import chromedriver_autoinstaller
+import undetected_chromedriver as uc
 import pickle
+import threading
+import re
 import csv
 import json
+from decouple import config
 import os
-from selenium_stealth import stealth
 
 class Scraper():
     
@@ -20,36 +23,107 @@ class Scraper():
     home_url = 'https://twitter.com/home'
     username_field_xpath = '//input[@name="text"]'
     password_field_xpath = '//input[@name="password"]'
-    if not os.path.isdir('ChromeDriver'):
-        os.mkdir('ChromeDriver')
-    chromedriver_path = chromedriver_autoinstaller.install(path='ChromeDriver')
+    
     output_path = 'ScrapedData'
     if not os.path.isdir(output_path):
         os.mkdir(output_path)
     output_path = f'{os.getcwd()}/{output_path}'
     
-    def __init__(self) -> None:
-        self.browser_options = webdriver.ChromeOptions()
-        self.browser_options.add_argument("--headless")
-        self.browser_options.add_argument("--start-maximized")
-        self.browser_options.add_argument('--disable-blink-features')
-        self.browser_options.add_argument('--disable-blink-features=AutomationControlled')
-        self.browser_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        self.browser_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        self.browser_options.add_experimental_option('useAutomationExtension', False)
-        self.driver = webdriver.Chrome(service=ChromeService(self.chromedriver_path), options=self.browser_options)
-        stealth(
-            self.driver,
-            user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-            languages=["en-GB","en-US", "en"],
-            vendor="Google Inc.",
-            platform="Linux x86_64",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            run_on_insecure_origins=True,
-        )
+    def __init__(self, enable_gui, start_sync) -> None:
+        self.enable_gui = enable_gui
+        self.start_sync = start_sync
+        self.keywords = [str(key).lower() for key in json.loads(open('search.json','r').read())['keywords']]
+        self.driver = self.create_session()
         self.driver.get(self.login_url)
+        time.sleep(3)
+    
+    def create_session(self) -> WebDriver:
+        browser_options = ChromeOptions()
+        if not self.enable_gui:
+            browser_options.add_argument('--headless')
+        browser_options.add_argument('--no-sandbox')
+        browser_options.add_argument('--start-maximized')
+        driver = uc.Chrome(options = browser_options)
+        return driver
+    
+    def dispose_session(self) ->None:
+        self.driver.quit()
+ 
+    def quick_login(self) -> bool:
+        print('Performing login using saved cookies')
+        try:
+            cookies = pickle.load(open('cookies.pkl', 'rb'))
+            for _ in range(3):
+                for cookie in cookies:
+                    self.driver.add_cookie(cookie)
+                self.driver.get(self.home_url)
+                if self.check_login():
+                    print('Login successful using saved cookie')
+                    return True
+        except Exception:
+            print('Error while performing quick login')
+        return False
+    
+    def extract_profile(self, user, info: list) -> None:
+        with open(f'ScrapedData/{user}.csv', "r+", newline='') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',')
+            all_users = list(row[1] for row in csvreader)
+            if info[1] not in all_users:
+                try:
+                    profile_driver = self.create_session()
+                    print(f'Fetching content from [{info[1]}]')
+                    profile_driver.get(info[1])
+                    try:
+                        time.sleep(3)
+                        link = json.loads(profile_driver.execute_script('return document.querySelector("[data-testid=UserProfileSchema-test]").text'))['relatedLink'][1]
+                        info.append(link)
+                    except Exception:
+                        info.append('NO URL FOUND')
+                    profile_driver.quit()
+                except Exception: print('Error while fetching user profile')
+            try:
+                info += re.findall(r"[A-Za-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", info[2])
+            except Exception: print('Error file searching Bio for emails')
+            print(info)
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(info)
+        
+    def standard_login(self, email: str, password: str, username: str) -> bool:
+        try:
+            print('Perform login using credentials provided in [.env] file')
+            self.driver.implicitly_wait(10)
+            username_field = self.driver.find_element(By.XPATH, self.username_field_xpath)
+            self.click_and_send_keys(username_field, email)
+            self.driver.execute_script('document.querySelectorAll("div[role=button]")[2].click()')
+            print('Email input successful')
+            time.sleep(3)
+            try:
+                username_field = self.driver.find_element(By.XPATH, self.username_field_xpath)
+                self.click_and_send_keys(username_field, username)
+                self.driver.execute_script('document.querySelectorAll("div[role=button]")[1].click()')
+                print('User Name input successful')
+            except Exception: pass
+            time.sleep(3)
+            password_field = self.driver.find_element(By.XPATH, self.password_field_xpath)
+            self.click_and_send_keys(password_field, password)
+            self.driver.find_element(By.XPATH, "//span[contains(text(), 'Log in')]").click()
+            print('Password input successful')
+            time.sleep(3)
+            try:
+                username_field = self.driver.find_element(By.XPATH, self.username_field_xpath)
+                otp = str(input(f'ENTER OTP RECEIVED EMAIL: [{email}] AND THEN PRESS [ENTER]: '))
+                self.click_and_send_keys(username_field, otp)
+                self.driver.execute_script('document.querySelectorAll("div[role=button]")[1].click()')
+                print('OTP input successful')
+            except Exception: pass
+            if self.check_login():
+                print('Login successful using provided credentials')
+                pickle.dump(self.driver.get_cookies(), open('cookies.pkl','wb'))
+                return True
+        except Exception:
+            print('Error while performing standard login')
+        print('Login not successfull')
+        return False
     
     def click_and_send_keys(self, target: WebElement, value: str):
         ActionChains(self.driver).click(target).perform()
@@ -58,28 +132,49 @@ class Scraper():
             time.sleep(0.1)
         time.sleep(2)
     
-    def quick_login(self) -> None:
-        print('Performing login using saved cookies')
-        time.sleep(10)
-        cookies = pickle.load(open('cookies.pkl', 'rb'))
-        for cookie in cookies:
-            self.driver.add_cookie(cookie)
-        self.driver.get(self.home_url)
-        print('Login successful')
-        
-    def standard_login(self, username: str, password: str) -> None:
-        self.driver.implicitly_wait(10)
-        self.driver.get(self.login_url)
-        username_field = self.driver.find_element(By.XPATH, self.username_field_xpath)
-        self.click_and_send_keys(username_field, username)
-        self.driver.execute_script('document.querySelectorAll("div[role=button]")[2].click()')
-        self.driver.implicitly_wait(10)
-        password_field = self.driver.find_element(By.XPATH, self.password_field_xpath)
-        self.click_and_send_keys(password_field, password)
-        self.driver.find_element(By.XPATH, "//span[contains(text(), 'Log in')]").click()
-        time.sleep(10)
-        pickle.dump(self.driver.get_cookies(), open('cookies.pkl','wb'))
+    def check_login(self) -> bool:
+        time.sleep(3)
+        try:
+            self.driver.execute_script('document.querySelector("h2[dir=ltr]").textContent')
+            return True
+        except Exception:
+            return False
     
+    def check_keywords(self, string):
+        for key in self.keywords:
+            if key in string:
+                return True
+        return False
+    
+    def get_previous_records(self, user):
+        old_user_names=[]
+        try:
+            with open(f'ScrapedData/{user}.csv', 'r') as csvfile:
+                csvreader = csv.DictReader(csvfile)
+                old_user_names = [user for user in [col['User_Name'] for col in csvreader]]
+                if len(old_user_names) == 0:
+                    csvfile.write('Name,User_Name,Bio,Website,Email\n')
+        except Exception:
+            with open(f'ScrapedData/{user}.csv', 'w') as csvfile:
+                csvfile.write('Name,User_Name,Bio,Website,Email\n')
+        return old_user_names
+    
+    def save_instance(self, info, user):
+        if self.check_keywords(str(info[2]).lower()):
+            if self.start_sync:
+                self.extract_profile(user, info)
+            else:
+                thread = threading.Thread(target=self.extract_profile, args=(user, info))
+                thread.start()
+                thread.join()
+    
+    def custom_scroll(self):
+        if self.first_scroll:
+            self.driver.execute_script('window.scrollTo(0,document.body.scrollHeight/4)')
+            self.first_scroll = False
+        else:
+            self.driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            
     def scrape_followers(self, user) -> None:
         follower_list=[]
         print(f'Navigating to [https://twitter.com/{user}/followers]')
@@ -89,14 +184,11 @@ class Scraper():
         )
         print('Followers ->')
         time.sleep(3)
-        first = True
+        self.first_scroll = True
         last_height = self.driver.execute_script("return document.body.scrollHeight")
+        old_user_names = self.get_previous_records(user)
         while True:
-            if first:
-                self.driver.execute_script('window.scrollTo(0,document.body.scrollHeight/4)')
-                first = False
-            else:
-                self.driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+            self.custom_scroll()
             time.sleep(3)
             page_source = self.driver.page_source
             soup = BeautifulSoup(page_source, 'html.parser')
@@ -105,13 +197,15 @@ class Scraper():
                 info=[]
                 try:
                     info.append(follower.find_all('a', {"role":"link"})[1].text)
-                    info.append(follower.find_all('a', {"role":"link"})[2].text)
+                    info.append(f'https://twitter.com/{follower.find_all("a", {"role":"link"})[2].text[1:]}')
+                    if info[1] in old_user_names:
+                        print(f'Skipping [{info[1]}] (Already in record)')
+                        continue
                     try:
                         info.append(follower.find_all('div', {"dir":"auto"})[1].text)
                     except Exception:
                         info.append('NO BIO FOUND')
-                    print(info)
-                    if info not in follower_list: follower_list.append(info)
+                    self.save_instance(info, user)
                 except Exception as e:
                     print(f'Error while fetching follower information -> {e}')
                     follower_list.append(['ERROR'])
@@ -120,6 +214,3 @@ class Scraper():
             if new_height == last_height:
                 break
             last_height = new_height
-        with open(f'ScrapedData/{user}.csv', 'w') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerows(follower_list)
--- setup.py
+++ setup.py
@@ -1,36 +1,31 @@
 from Scraper.TwitterScraper import Scraper as TwitterScraper
-from ConfigCelery.CeleryWorker import CeleryWorker
-import os
+import json
 import argparse
 from decouple import config
 
+parser = argparse.ArgumentParser(
+    prog = 'Twitter Follower Scraper',
+)
+parser.add_argument('--enable-gui', action='store_true')
+parser.add_argument('--start-sync', action='store_true')
 
-worker = CeleryWorker(config('REDIS_URL'))
-task_routes = (['Twitter Scraper','Twitter Queue'])
-module_locations = ['setup']
-worker.discover_and_set_task_routes(task_routes, module_locations)
-
-def parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--user")
-    return parser.parse_args()
-
-@worker.app.task(name='Twitter Scraper')
-def pusher(user):
-    controller = TwitterScraper()
-    if os.path.isfile('cookies.pkl'):
-        print('Found cookies from a previous session')
-        controller.quick_login()
-    else:
-        print('no')
-        controller.standard_login(config('TWITTER_USER'), config('TWITTER_PASS'))
-    controller.scrape_followers(user)
+def twitter_main(user, enable_gui, start_sync):
+    controller = TwitterScraper(enable_gui, start_sync)
+    login_success = controller.quick_login()
+    if not login_success:
+        login_success = controller.standard_login(
+            config('TWITTER_USER'),
+            config('TWITTER_PASS'),
+            config('TWITTER_USER_NAME')
+        )
+    if login_success:
+        controller.scrape_followers(user)
+    controller.dispose_session()
+    print(f'successfully finished scraping [{user}]')
 
 if __name__ == '__main__':
-    args = parser()
-    user = args.user
-    if user:
-        # worker.define_worker_arguments()
-        # worker.app.worker_main(worker.start_args)
-        # pusher.delay(args=([user]))
-        pusher(user)
+    args = parser.parse_args()
+    users = json.loads(open('search.json','r').read())['users']
+    for user in users:
+        print(f'[USER] -> [{user}]')
+        twitter_main(user, args.enable_gui, args.start_sync)
